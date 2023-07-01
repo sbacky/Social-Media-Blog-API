7b33d6acc4b2c0b676f14e4e31430e9e0a81e8fc
Neil Crum <nhc.crum@outlook.com>
2023-07-01 02:08:21 +0000

    Removed unecessary import

--- src/main/java/DAO/AccountDAOImpl.java
+++ src/main/java/DAO/AccountDAOImpl.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Comparator;
+// import java.util.Comparator;
 import java.util.List;
 
 import Model.Account;
