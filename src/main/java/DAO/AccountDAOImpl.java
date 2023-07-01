3005e5bac2c02b288900862ef26319953e716d17
Neil Crum <nhc.crum@outlook.com>
2023-07-01 00:50:26 +0000

    Implemented Account and Message DAO's

---
+++ src/main/java/DAO/AccountDAOImpl.java
@@ -0,0 +1,122 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAOImpl implements AccountDAO {
+
+    private Connection conn = null;
+    private PreparedStatement ps = null;
+
+    @Override
+    public List<Account> getAccounts() {
+        List<Account> accounts = new ArrayList<>();
+
+        try {
+            conn = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM account";
+            ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int accountID = rs.getInt("account_id");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+
+                Account account = new Account(accountID, username, password);
+                accounts.add(account);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+        return accounts;
+    }
+
+    @Override
+    public Account getAccountByID(int id) {
+        Account account = null;
+
+        try {
+            conn = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM account WHERE account_id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int accountID = rs.getInt("account_id");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+
+                account = new Account(accountID, username, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+        return account;
+    }
+
+    @Override
+    public Account addAccount(Account account) {
+        String username = account.getUsername();
+        String password = account.getPassword();
+
+        try {
+            conn = ConnectionUtil.getConnection();
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+            ps = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+
+            if (ps.executeUpdate() != 0) {
+                ResultSet keys = ps.getGeneratedKeys();
+                while (keys.next()) {
+                    int accountID = keys.getInt("account_id");
+                    account.setAccount_id(accountID);
+                    return account;
+                }
+                /*List<Account> accounts = getAccounts();
+                return accounts.stream().max(Comparator.comparingInt(Account::getAccount_id)).get();*/
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+        return null;
+    }
+
+    private void closeResources() {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ src/main/java/DAO/MessageDAOImpl.java
@@ -0,0 +1,165 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Message;
+import Util.ConnectionUtil;
+
+public class MessageDAOImpl implements MessageDAO {
+
+    private Connection conn = null;
+    private PreparedStatement ps = null;
+
+    @Override
+    public List<Message> getMessages() {
+        List<Message> messages = new ArrayList<>();
+
+        try {
+            conn = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM message";
+            ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int messageID = rs.getInt("message_id");
+                int postedBy = rs.getInt("posted_by");
+                String messageText = rs.getString("message_text");
+                long timePostedEpoch = rs.getLong("time_posted_epoch");
+
+                Message message = new Message(messageID, postedBy, messageText, timePostedEpoch);
+                messages.add(message);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+
+        return messages;
+    }
+
+    @Override
+    public List<Message> getMessagesForUser(int id) {
+        List<Message> messages = new ArrayList<>();
+
+        try {
+            conn = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM message WHERE posted_by = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int messageID = rs.getInt("message_id");
+                int postedBy = rs.getInt("posted_by");
+                String messageText = rs.getString("message_text");
+                long timePostedEpoch = rs.getLong("time_posted_epoch");
+
+                Message message = new Message(messageID, postedBy, messageText, timePostedEpoch);
+                messages.add(message);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+
+        return messages;
+    }
+
+    @Override
+    public Message getMessageByID(int id) {
+        Message message = null;
+
+        try {
+            conn = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM message WHERE message_id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int messageID = rs.getInt("message_id");
+                int postedBy = rs.getInt("posted_by");
+                String messageText = rs.getString("message_text");
+                long timePostedEpoch = rs.getLong("time_posted_epoch");
+
+                message = new Message(messageID, postedBy, messageText, timePostedEpoch);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+        return message;
+    }
+
+    @Override
+    public Message addMessage(Message message) {
+        int postedBy = message.getPosted_by();
+        String messageText = message.getMessage_text();
+        long timePostedEpoch = message.getTime_posted_epoch();
+
+        try {
+            conn = ConnectionUtil.getConnection();
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?)";
+            ps = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, postedBy);
+            ps.setString(2, messageText);
+            ps.setLong(3, timePostedEpoch);
+
+            if (ps.executeUpdate() != 0) {
+                ResultSet keys = ps.getGeneratedKeys();
+                while (keys.next()) {
+                    int messageID = keys.getInt("message_id");
+                    message.setMessage_id(messageID);
+                    return message;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources();
+        }
+        return null;
+    }
+
+    @Override
+    public Message updateMessage(Message message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateMessage'");
+    }
+
+    @Override
+    public boolean deleteMessage(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteMessage'");
+    }
+
+    private void closeResources() {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
