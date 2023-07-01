72431d3e5fd7803adb165964dbc8b9b1e9e00a22
Neil Crum <nhc.crum@outlook.com>
2023-07-01 23:12:27 +0000

    Add services for DAO implementations

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,73 @@
+package Service;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import DAO.AccountDAO;
+import DAO.AccountDAOImpl;
+import Model.Account;
+
+public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService() {
+        this.accountDAO = new AccountDAOImpl();
+    }
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    /**
+     * Verify account username and password are a valid combination and
+     * return verified account with account_id or null if not valid.
+     * @param account (Username and password only!)
+     * @return account with account_id
+     */
+    public Account loginAccount(Account account) {
+        String username = account.getUsername();
+        String password = account.getPassword();
+        // Check if username and password is valid
+        Optional<Account> checkAccount = accountDAO.getAccounts().stream()
+                                .filter(a -> Objects.equals(a.getUsername(), username) && Objects.equals(a.getPassword(), password))
+                                .reduce((a, b) -> {throw new IllegalStateException();});
+        // Return account if valid or null otherwise
+        if (checkAccount.isPresent()) {
+            return checkAccount.get();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Create account if username and password are valid.<br><br>
+     * Username is valid if field is not blank and username has not
+     * been used by another account.
+     * Password is valid if it is 4 characters or longer.
+     * @param account (Username and passowrd only!)
+     * @return account with account_id
+     */
+    public Account createAccount(Account account) {
+        String username = account.getUsername();
+        String password = account.getPassword();
+        // Check if username is blank
+        if (username.isBlank()) {
+            return null;
+        }
+        // Check if password is atleast 4 characters long
+        if (password.length() < 4) {
+            return null;
+        }
+        // Check if username has not already been used by another accoount
+        Optional<Account> checkAccount = accountDAO.getAccounts().stream()
+                                .filter(a -> Objects.equals(a.getUsername(), username))
+                                .reduce((a, b) -> {throw new IllegalStateException();});
+        // Creat and return account with account_id if username and password are valid, null otherwise
+        if (checkAccount.isEmpty()) {
+            return accountDAO.addAccount(account);
+        } else {
+            return null;
+        }
+    }
+    
+}
---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,119 @@
+package Service;
+
+import java.util.List;
+
+import DAO.AccountDAO;
+import DAO.AccountDAOImpl;
+import DAO.MessageDAO;
+import DAO.MessageDAOImpl;
+import Model.Message;
+
+public class MessageService {
+    private MessageDAO messageDAO;
+    private AccountDAO accountDAO;
+
+    public MessageService() {
+        this.messageDAO = new MessageDAOImpl();
+        this.accountDAO = new AccountDAOImpl();
+    }
+
+    public MessageService(MessageDAO messageDAO) {
+        this.messageDAO = messageDAO;
+        this.accountDAO = new AccountDAOImpl();
+    }
+
+    public MessageService(MessageDAO messageDAO, AccountDAO accountDAO) {
+        this.messageDAO = messageDAO;
+        this.accountDAO = accountDAO;
+    }
+
+    public MessageService(AccountDAO accountDAO) {
+        this.messageDAO = new MessageDAOImpl();
+        this.accountDAO = accountDAO;
+    }
+
+    /**
+     * Get list of all messages
+     * @return List of messages or empty list if no messages
+     */
+    public List<Message> getAllMessages() {
+        return messageDAO.getMessages();
+    }
+
+    /**
+     * Get list of messages for user at accountID
+     * @param accountID
+     * @return list of messages for accountID or empty list if no messages
+     * for accountID
+     */
+    public List<Message> getMessagesForUser(int accountID) {
+        return messageDAO.getMessagesForUser(accountID);
+    }
+
+    /**
+     * Get message at messageID
+     * @param messageID
+     * @return message at messageID or null if no message at messageID found
+     */
+    public Message getMessageByID(int messageID) {
+        return messageDAO.getMessageByID(messageID);
+    }
+
+    /**
+     * Create new message if postedBy and messageText are valid.<br><br>
+     * postBy is valid if it refers to a real, existing user.
+     * messageText is valid if it is not blank and less than 255 characters.
+     * @param message (postedBy, messageText and timePostedEpoch only!)
+     * @return added message with messageID or null if message wasnt added
+     */
+    public Message createMessage(Message message) {
+        int postedBy = message.getPosted_by();
+        String messageText = message.getMessage_text();
+        // Check if message is blank or longer than 254 characters
+        if (messageText.isBlank() || messageText.length() > 254) {
+            return null;
+        }
+        // Check if postedBy refers to a real, existing user
+        if (accountDAO.getAccountByID(postedBy) == null) {
+            return null;
+        }
+        // Add and return message with messageID
+        return messageDAO.addMessage(message);
+    }
+
+    /**
+     * Update message if messageID and messageText are valid.<br><br>
+     * messageID is valid if it refers to an existing message.
+     * messageText is valid if it is not blank and is less than 255 characters.
+     * @param message (messageID and messageText only!)
+     * @return message with updated messageText or null if message was
+     * not updated.
+     */
+    public Message updateMessage(Message message) {
+        int messageID = message.getMessage_id();
+        String messageText = message.getMessage_text();
+        // Check if messageID refers to an existing message
+        if (messageDAO.getMessageByID(messageID) == null) {
+            return null;
+        }
+        // Check if messageText is blank or longer than 254 characters
+        if (messageText.isBlank() || messageText.length() > 254) {
+            return null;
+        }
+        return messageDAO.updateMessage(message);
+    }
+
+    /**
+     * Delete message at messageID if a message exists at messageID.
+     * @param messageID
+     * @return true if message at messageID is deleted, false otherwise.
+     */
+    public boolean deleteMessage(int messageID) {
+        // Check if messageID refers to an existing message
+        if (messageDAO.getMessageByID(messageID) == null) {
+            return false;
+        }
+        return messageDAO.deleteMessage(messageID);
+    }
+
+}
